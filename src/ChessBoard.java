656d63644db7dcb0a76a6927ec2ff49673b6500a
Nguyễn Quí Vinh Quang <60070577+nguyenquivinhquang@users.noreply.github.com>
2020-09-28 22:43:29 +0700

    Add files via upload

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -1,31 +1,24 @@
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.lang.reflect.Array;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.Arrays;
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
+
 
 public class ChessBoard {
     private JFrame mainFrame;
-    private JLabel headerLabel;
-    private JLabel statusLabel;
-    private JPanel controlPanel;
+
     public int row = 8, column = 8;
-    private int player = 0; // 0: player 1, 1: player 2.
     public int[][] move = new int[row][column];
-    private Boolean[][] fee = new Boolean[100][100];
-    private GamePlay game = new GamePlay();
     public int[][] posibleMove = new int[row][column];
+
+    private int player = 0, p1Score = 0, p2Score = 0; // 0: player 1, 1: player 2.
 
     public ChessBoard() {
         prepareGUI();
-
     }
 
     public static void main(String[] args) {
@@ -33,15 +26,18 @@ public class ChessBoard {
     }
 
     public JButton b[][] = new JButton[column + 2][row + 2];
+
     Icon icon1 = new ImageIcon("D:\\java\\MyCode\\src\\Image\\cross.gif");
     Icon icon2 = new ImageIcon("D:\\java\\MyCode\\src\\Image\\nought.gif");
-    Icon iconPosibleMove = new ImageIcon("D:\\java\\MyCode\\src\\Image\\a.png");
+    Icon iconPosibleMove = new ImageIcon("D:\\java\\Reversi\\src\\Image\\a.jpg");
+
     private JPanel scoreMenu = new JPanel();
     private JPanel panecenter = new JPanel();
-    JPanel panelEast = new JPanel();
+
+    JLabel player1Score = new JLabel("Player 1 score: 0");
+    JLabel player2Score = new JLabel("Player 2 score: 0");
 
     public void prepareGUI() {
-//        Arrays.fill(fee[0], Boolean.TRUE);
         for (int i = 0; i < 8; i++)
             Arrays.fill(move[i], -1);
         mainFrame = new JFrame("ChessBoard");
@@ -52,22 +48,9 @@ public class ChessBoard {
                 System.exit(0);
             }
         });
-        scoreMenu.setLayout(null);
-
-        JLabel player1Score = new JLabel("Player 1 score: ");
-        JLabel player2Score = new JLabel("Player 2 score: ");
-
-        scoreMenu.add(player1Score);
-        scoreMenu.add(player2Score);
-
-        player1Score.setBounds(100, 50, 130, 20);
-        player2Score.setBounds(100, 80, 130, 20);
 
         panecenter.setLayout(new GridLayout(8, 8));
-
         mainFrame.add(panecenter, BorderLayout.WEST);
-        mainFrame.add(scoreMenu);
-
         for (int i = 0; i < column; i++)
             for (int j = 0; j < row; j++) {
                 b[i][j] = new JButton();
@@ -76,7 +59,16 @@ public class ChessBoard {
                 b[i][j].addActionListener(this::actionPerformed);
                 panecenter.add(b[i][j]);
             }
+        scoreMenu.setLayout(null);
 
+
+        scoreMenu.add(player1Score);
+        scoreMenu.add(player2Score);
+
+        player1Score.setBounds(90, 50, 130, 20);
+        player2Score.setBounds(90, 80, 130, 20);
+
+        mainFrame.add(scoreMenu);
         panecenter.setSize(900, 900);
         System.out.println("hihi");
         mainFrame.setVisible(true);
@@ -93,12 +85,17 @@ public class ChessBoard {
 
         if (player == 1) b[i][j].setIcon(icon1);
         else b[i][j].setIcon(icon2);
+
         move[i][j] = player;
-        player = (player + 1) % 2;
-        System.out.println("Runing into this function");
+
+        p1Score  = GamePlay.CountPlayer1Score(move);
+        p2Score  = GamePlay.CountPlayer2Score(move);
+        player1Score.setText("Player 1 score: "+ p1Score);
+        player2Score.setText("Player 2 score: "+ p2Score);
 
         posibleMove = GamePlay.CheckPosibleMove(move, player);
-        //DrawPosibleMove();
+        player = (player + 1) % 2;
+//        /DrawPosibleMove();
     }
 
     public void DrawPosibleMove() {
@@ -106,7 +103,7 @@ public class ChessBoard {
         for (int i = 0; i < 8; i++)
             for (int j = 0; j < 8; j++) {
                 if (posibleMove[i][j] != -1) {
-                    b[i][j].setIcon(iconPosibleMove);
+                    //b[i][j].setIcon(iconPosibleMove);
                     panecenter.add(b[i][j]);
                     System.out.println("running");
                 }
--- src/GamePlay.java
+++ src/GamePlay.java
@@ -1,13 +1,10 @@
-import java.io.*;
-import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.Scanner;
+
 
 // Singleton design pattern
 
 public class GamePlay {
     public static int row = 8, column = 8;
-    private static int number = 1;
 
     public static int[][] CheckPosibleMove(int[][] check, int player) { //0: player 1, 1 player 2
         int[][] posibleMove = new int[row + 1][column + 1]; // 1 posible move, 2 posible move, 0 not
@@ -24,19 +21,19 @@ public class GamePlay {
                     if (check[i][j] == player) horizontal[i][j] = player;
                     else horizontal[i][j] = horizontal[i][j - 1];
                 }
-                if (i - 1 >= 0) {
-                    if (check[i][j] == player) horizontal[i][j] = player;
-                    else horizontal[i][j] = horizontal[i - 1][j];
-                }
+//                if (i - 1 >= 0) {
+//                    if (check[i][j] == player) horizontal[i][j] = player;
+//                    else horizontal[i][j] = horizontal[i - 1][j];
+//                }
             }
         }
-        System.out.println("GL");
+
         for (int i = 0; i < 8; i++)
             for (int j = 0; j < 8; j++) {
                 if (i - 1 >= 0 && check[i][j] == -1 && horizontal[i - 1][j] == player)
                     posibleMove[i][j] = 1;
-                if (j - 1 >= 0 && check[i][j] == -1 && horizontal[i][j - 1] == player)
-                    posibleMove[i][j] = 1;
+//                if (j - 1 >= 0 && check[i][j] == -1 && horizontal[i][j - 1] == player)
+//                    posibleMove[i][j] = 1;
             }
         // debug
         for (int i = 0; i < row; i++) {
@@ -48,22 +45,22 @@ public class GamePlay {
         return posibleMove;
     }
 
-    public static void InputFile() {
-        try {
-            File x = new File("D:\\java\\Reversi\\src\\hihi.txt");
-            Scanner sc = new Scanner(x);
-            String content = "";
-            while (sc.hasNextLine()) {
-                content += sc.nextLine() + "\r\n";
-            }
-            System.out.println(content);
 
-            sc.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Error");
-        }
+
+    public static int CountPlayer1Score(int[][] board) {
+        int p1Score = 0;
+        for (int i = 0; i < row; i++)
+            for (int j = 1; j < column; j++)
+                if (board[i][j] == 0) p1Score++;
+        return p1Score;
     }
-
+    public static int CountPlayer2Score(int[][] board) {
+        int p2Score = 0;
+        for (int i = 0; i < row; i++)
+            for (int j = 1; j < column; j++)
+                if (board[i][j] == 1) p2Score++;
+        return p2Score;
+    }
 
 //    public static void main(String[] args) {
 //        int[][] arr = new int[8][8];
@@ -74,3 +71,9 @@ public class GamePlay {
 //        //CheckPosibleMove(arr);
 //    }
 }
+// debug
+// for (int i = 0; i < row; i++) {
+//        for (int j = 1; j < column; j++)
+//        System.out.print(posibleMove[i][j] + " ");
+//        System.out.println();
+//        }
