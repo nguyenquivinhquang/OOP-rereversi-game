e543de1b59126a57abefd29e0438dbd007681a40
Nguyễn Quí Vinh Quang <60070577+nguyenquivinhquang@users.noreply.github.com>
2020-12-29 14:00:33 +0700

    Update Gui.java

--- src/Gui.java
+++ src/Gui.java
@@ -1,49 +1,9 @@
-
-
-import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
-
-import GUI.*;
-
 import javax.swing.*;
 
-public class Gui extends JPanel {
-    int x, y;
-    JFrame frame = new JFrame("Reversi");
+public class GUI extends JFrame {
+    JFrame frame = new JFrame();
 
-    public Gui() {
+    GUI() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        RenderChessboard board = new RenderChessboard(x, y);
-
-        frame.add(board);
-
-        frame.setIconImage(Toolkit.getDefaultToolkit().getImage(Parameter.logo));
-        board.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                getRowColumn(e.getX(), e.getY());
-                System.out.println(x + " " + y);
-                board.setXY(x, y);
-
-            }
-        });
-        frame.setSize(1000, 700);
-        frame.setVisible(true);
-    }
-
-    public void getRowColumn(int x, int y) {
-        int si = (int) (Double.valueOf(Parameter.size) / 8.0);
-        this.x = x / si + 1;
-        this.y = y / si + 1;
-        System.out.println(this.x + " " + this.y);
-    }
-    private void closeProgram() {
-        frame.dispose();
-    }
-    public static void main(String[] args) {
-        Gui gui = new Gui();
     }
 }
