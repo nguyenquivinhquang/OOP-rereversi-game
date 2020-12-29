342da01282ef5a7354ca70a2c95f013093d9393c
huynhtrucquyen <69337840+huynhtrucquyen@users.noreply.github.com>
2020-12-29 13:49:13 +0700

    add music

---
+++ src/Audio/PlayingAudioFileTest.java
@@ -0,0 +1,57 @@
+package Audio;
+
+import javax.sound.sampled.*;
+import java.io.IOException;
+import java.io.File;
+
+public class PlayingAudioFileTest {
+    long musicCurrentTime;
+    Clip music;
+
+
+    AudioInputStream musicInput;
+    public static String filePath = "Audio/perry.wav";
+
+    public PlayingAudioFileTest() {
+        try {
+            musicInput = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            music = AudioSystem.getClip();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+        try {
+            music.open(musicInput);
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        music.loop(Clip.LOOP_CONTINUOUSLY);
+
+    }
+
+    public void playMusic() {
+        music.start();
+        music.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void pauseMusic() {
+        this.musicCurrentTime = this.music.getMicrosecondPosition();
+        music.stop();
+    }
+
+    public void resumeMusic() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+
+        music.setMicrosecondPosition(musicCurrentTime);
+        this.playMusic();
+    }
+
+
+
+}
