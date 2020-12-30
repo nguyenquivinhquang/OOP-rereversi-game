da53881374a56e69918e30b5d941911c18506260
Nguyễn Quí Vinh Quang <60070577+nguyenquivinhquang@users.noreply.github.com>
2020-12-30 23:14:55 +0700

    Update RegionScore for Minimax

--- src/Core/RegionScore.java
+++ src/Core/RegionScore.java
@@ -6,70 +6,20 @@ public class RegionScore {
     private static int[][] regionScore = new int[row + 2][column + 2];
 
     private static void setRegionScore() {
-        regionScore[1][1] = 100;
-        regionScore[1][2] = -70;
-        regionScore[1][3] = 70;
-        regionScore[1][4] = 70;
-        regionScore[1][5] = 70;
-        regionScore[1][6] = 70;
-        regionScore[1][7] = -70;
-        regionScore[1][8] = 100;
-        regionScore[2][1] = -50;
-        regionScore[2][2] = -50;
-        regionScore[2][3] = -30;
-        regionScore[2][4] = -30;
-        regionScore[2][5] = -30;
-        regionScore[2][6] = -30;
-        regionScore[2][7] = -70;
-        regionScore[2][8] = -70;
-        regionScore[3][1] = 70;
-        regionScore[3][2] = -70;
-        regionScore[3][3] = 20;
-        regionScore[3][4] = 20;
-        regionScore[3][5] = 20;
-        regionScore[3][6] = 20;
-        regionScore[3][7] = -70;
-        regionScore[3][8] = 70;
-        regionScore[4][1] = 70;
-        regionScore[4][2] = -70;
-        regionScore[4][3] = 20;
-        regionScore[4][4] = 20;
-        regionScore[4][5] = 20;
-        regionScore[4][6] = 20;
-        regionScore[4][7] = -70;
-        regionScore[4][8] = 70;
-        regionScore[5][1] = 70;
-        regionScore[5][2] = -70;
-        regionScore[5][3] = 20;
-        regionScore[5][4] = 20;
-        regionScore[5][5] = 20;
-        regionScore[5][6] = 20;
-        regionScore[5][7] = -70;
-        regionScore[5][8] = 70;
-        regionScore[6][1] = 70;
-        regionScore[6][2] = -70;
-        regionScore[6][3] = 20;
-        regionScore[6][4] = 20;
-        regionScore[6][5] = 20;
-        regionScore[6][6] = 20;
-        regionScore[6][7] = -70;
-        regionScore[6][8] = 70;
-        regionScore[7][1] = -70;
-        regionScore[7][2] = -70;
-        regionScore[7][3] = -30;
-        regionScore[7][4] = -30;
-        regionScore[7][5] = -30;
-        regionScore[7][6] = -30;
-        regionScore[7][7] = -70;
-        regionScore[7][8] = -70;
-        regionScore[8][1] = 100;
-        regionScore[8][2] = -70;
-        regionScore[8][3] = 70;
-        regionScore[8][4] = 70;
-        regionScore[8][5] = 70;
-        regionScore[8][6] = 70;
-        regionScore[8][7] = -70;
-        regionScore[8][8] = 100;
+        int[][] temp =
+                {
+                        {20, -3, 11, 8, 8, 11, -3, 20},
+                        {-3, -7, -4, 1, 1, -4, -7, -3},
+                        {11, -4, 2, 2, 2, 2, -4, 11},
+                        {8, 1, 2, -3, -3, 2, 1, 8},
+                        {8, 1, 2, -3, -3, 2, 1, 8},
+                        {11, -4, 2, 2, 2, 2, -4, 11},
+                        {-3, -7, -4, 1, 1, -4, -7, -3},
+                        {20, -3, 11, 8, 8, 11, -3, 20}
+                };
+        for (int i = 1; i <= row; i++)
+            for (int j = 1; j <= column; j++)
+                regionScore[i][j] = temp[i - 1][j - 1];
     }
 
     public static int[][] getRegionScore() {
