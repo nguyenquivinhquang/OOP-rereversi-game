398f671a3c0862df12ae951cebe7918da93dc428
huynhtrucquyen <69337840+huynhtrucquyen@users.noreply.github.com>
2021-01-02 23:06:27 +0700

    Update Rules
    Add Jbutton to comback main menu and dispose

--- src/MenuComponent/Rules.java
+++ src/MenuComponent/Rules.java
@@ -1,40 +1,124 @@
-package MenuComponent;
-
-import java.awt.Graphics;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.imageio.ImageIO;
-import javax.swing.*;
 
 
-public class Rules extends JPanel {
-
-    private BufferedImage image;
+public class Rules extends JFrame {
 
     public Rules() {
+        this.setTitle("Reversi");
+        initComponents();
+        setSize(450, 850);
+    }
+
+                   
+    private void initComponents() {
+
+        jPanel1 = new drawRules(this.getWidth(), this.getHeight());
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setAutoRequestFocus(false);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 153));
+
+        jButton1.setBackground(new java.awt.Color(255, 204, 0));
+        jButton1.setForeground(new java.awt.Color(255, 153, 0));
+        jButton1.setText("OK");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(152, 152, 152)
+                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(180, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addGap(0, 286, Short.MAX_VALUE)
+                                .addComponent(jButton1))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }            
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
+        dispose();
+    }
+
+
+    public static void main(String args[]) {
+   
         try {
-            image = ImageIO.read(new File("gamerules-1.jpg"));
-        } catch (IOException ex) {
-            Logger.getLogger(Rules.class.getName()).log(Level.SEVERE, null, ex);
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Rules.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Rules.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Rules.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Rules.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Rules().setVisible(true);
+            }
+        });
     }
 
-    @Override
-    protected void paintComponent(Graphics g) {
-//        super.paintComponent(g);
-        g.drawImage(image, 2000, 2000, 100, 100, null); // see javadoc for more info on the parameters
+                 
+    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel jPanel1;
+           
+}
+
+
+class drawRules extends  JPanel {
+    int width, height;
+    private BufferedImage image;
+    public drawRules(int width, int height) {
+        this.width = width;
+        this.height = height;
     }
-
-    public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        Rules rules = new Rules();
-
-        frame.add(rules);
-        frame.setVisible(true);
+    public void paintComponent(Graphics g) {
+        try {
+            image = ImageIO.read(new File("E:\\java\\OOP-rereversi-game\\Image\\gamerules.jpg"));
+        } catch (IOException ex) {
+            System.out.println("error");
+        }
+        g.setColor(new java.awt.Color(255, 255, 153));
+        g.fillRect(1,1, 1000,1000);
+        System.out.println(width);
+        g.drawImage(image, 40, 0, 350, 770, null); // see javadoc for more info on the parameters
     }
-
-
 }
