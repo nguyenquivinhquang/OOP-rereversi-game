866e811a0d778fd8a8d15f21acf0b61c7f3894a5
Nguyễn Quí Vinh Quang <60070577+nguyenquivinhquang@users.noreply.github.com>
2021-01-02 23:03:19 +0700

    Update Gui.java

--- src/GUI/Gui.java
+++ src/GUI/Gui.java
@@ -2,76 +2,103 @@ package GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import MenuComponent.*;
 import Audio.PlayingAudioFileTest;
 import Core.*;
 
 public class Gui extends JFrame {
 
-
     public PlayerVsPlayer playerVsPlayer = PlayerVsPlayer.getInstance();
     public PlayervsBot playervsBot = PlayervsBot.getInstance();
-
     PlayingAudioFileTest music;
-
+    int curplay = 0; // 1 is bot, 2 is player
     JMenuBar menuBar = new JMenuBar();
     JMenu gameOptionsMenu = new JMenu(" Game options");
     JMenu musicMenu = new JMenu("Music options");
-
+    JMenu mainMenu = new JMenu("Menu");
     JMenuItem exitJMenu = new JMenuItem("Exits");
     JMenuItem newGameJMenu = new JMenuItem("New Games");
     JMenuItem playMusic = new JMenuItem("Play");
     JMenuItem stopMusic = new JMenuItem("Stop");
-
+    JMenuItem backToMenu = new JMenuItem("Back to Menu");
     public Gui() {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setIconImage(Toolkit.getDefaultToolkit().getImage(Parameter.logo));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setIconImage(Toolkit.getDefaultToolkit().getImage(Parameter.logo));
         setTitle("Reversi");
 
-        music = new PlayingAudioFileTest();
-//        this.add(playerVsPlayer.render);
-//        playerVsPlayer.actionGame();
+        music = PlayingAudioFileTest.getInstance();
+
         setMenu();
-        this.setSize(1000, 700);
-        this.setVisible(true);
-}
+        setSize(1000, 700);
+        setVisible(true);
+    }
 
     public void startPlayervsPlayer() {
-//        PlayerVsPlayer playerVsPlayer = PlayerVsPlayer.getInstance();
+        curplay = 2;
+        add(playerVsPlayer.render);
+        playerVsPlayer.running();
+        music.resumeMusic();
 
-        this.add(playerVsPlayer.render);
-        repaint();
-        playerVsPlayer.actionGame();
-//        playerVsPlayer.render.repaint();
     }
 
     public void startPlayervsBot() {
-        this.add(playervsBot);
-        playervsBot.actionGame();
+        curplay = 1;
+        add(playervsBot.render);
+        playervsBot.running();
+        music.resumeMusic();
     }
-    public void newGameAction() {
+
+    private void setNewGameJMenu() {
         newGameJMenu.addActionListener(e -> {
-            playerVsPlayer.resetArray();
-            playerVsPlayer.render.repaint();
+            if (curplay == 2)
+                playerVsPlayer.newGame();
+            else playervsBot.newGame();
+
             music.resumeMusic();
             System.out.println("new Game");
         });
     }
 
+    private void setBackToMenu() {
+        backToMenu.addActionListener(e -> {
+            MenuView.instance.setVisible(true);
+            this.dispose();
+            music.extMusic();
+        });
+    }
+
+    private void setStopMusic() {
+        stopMusic.addActionListener(e -> music.pauseMusic());
+    }
+
+    private void setPlayMusic() {
+        playMusic.addActionListener(e -> music.playMusic());
+    }
+    private void setExitJMenu() {
+        exitJMenu.addActionListener(e -> System.exit(0));
+    }
     private void setMenu() {
         menuBar.add(gameOptionsMenu);
         menuBar.add(musicMenu);
+        menuBar.add(mainMenu);
         menuBar.setBackground(Color.yellow);
-        this.setJMenuBar(menuBar);
+
+        setJMenuBar(menuBar);
+
+        mainMenu.add(backToMenu);
 
         gameOptionsMenu.add(newGameJMenu);
         gameOptionsMenu.add(exitJMenu);
-        exitJMenu.addActionListener(e -> this.dispose());
-        newGameAction();
 
-        System.out.println("setmenu");
-        playMusic.addActionListener(e -> music.playMusic());
+        setBackToMenu();
+        setExitJMenu();
+        setNewGameJMenu();
+        setPlayMusic();
+        setStopMusic();
 
-        stopMusic.addActionListener(e -> music.pauseMusic());
+
         musicMenu.add(playMusic);
         musicMenu.add(stopMusic);
     }
@@ -80,13 +107,10 @@ public class Gui extends JFrame {
         Gui gui = new Gui();
         gui.startPlayervsPlayer();
     }
+
     public static void newGamePlayervsBot() {
         Gui gui = new Gui();
         gui.startPlayervsBot();
     }
-    public static void main(String[] args) {
-        Gui gui = new Gui();
-        gui.startPlayervsPlayer();
 
-    }
 }
